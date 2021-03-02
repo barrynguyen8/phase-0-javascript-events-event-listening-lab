42156ebb19ea4e4f8e9b78dcd8825fda9f134e94
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-03-02 16:45:43 +1100

    assignment submitted

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+  const div = document.getElementById('div5');
+  
+  function clickAlert() {
+    alert('I was clicked!');
+  
+  }
+  
+  input.addEventListener('click', clickAlert);
+  div.addEventListener('click', clickAlert);
+
 }
+
+
+
+
+
+
